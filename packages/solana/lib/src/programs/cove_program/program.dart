ec2e22a95ac8a7e00226c8bc3bfbf90e00329bfe
Daniel Boyer <danielkboyer@gmail.com>
2024-07-11 15:21:48 -0600

    Export solana_client_ext.dart for cove program.

--- packages/solana/lib/src/programs/cove_program/program.dart
+++ packages/solana/lib/src/programs/cove_program/program.dart
@@ -1,6 +1,7 @@
 import 'package:solana/src/crypto/ed25519_hd_public_key.dart';
 
 export 'instruction.dart';
+export 'solana_client_ext.dart';
 
 abstract class CoveProgram {
   static const programId = '2zG2G696WnzNVPzP7MCPgQ5fJZnhYDsR71UUHCB4fdfW';
